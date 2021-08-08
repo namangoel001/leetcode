060465dac1d671a2cb485bab2730c876ddef1ffe
naman goel <51822476+namangoel001@users.noreply.github.com>
2021-08-08 18:31:18 +0530

    Time: 4 ms (87.25%), Space: 9.9 MB (49.88%) - LeetHub

---
+++ gas-station/gas-station.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+     int total_surplus=0;
+     int surplus=0;
+     int s=0;
+     for(int i=0;i<gas.size();i++)
+     {
+         total_surplus+=gas[i]-cost[i];
+         surplus+=gas[i]-cost[i];
+         if(surplus<0)
+         {
+             surplus=0;
+             s=i+1;
+         }
+     }
+    return total_surplus<0 ? -1 :s;
+    }
+};
