7b04f024e49b7ffade93ebacb023b46be64737ff
naman goel <51822476+namangoel001@users.noreply.github.com>
2021-08-18 02:27:34 +0530

    Time: 0 ms (100.00%), Space: 13.1 MB (53.40%) - LeetHub

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -2,22 +2,14 @@ class Solution {
 public:
     int maxSubArray(vector<int>& a) 
     {
-    int i=0;
     int sum=0;
     int max1=INT_MIN;
-	while(i<a.size() && a[i]<0)
+    for(int j=0;j<a.size();j++)
     {
-     max1=max(max1,a[i]);
-         i++;
-    }
-    if(i==a.size())
-        return max1;
-    for(int j=i;j<a.size();j++)
-    {
-        if(sum<0)
-            sum=0;
         sum+=a[j];
         max1=max(max1,sum);
+        if(sum<0)
+          sum=0;
     }
         return max1;
     }
