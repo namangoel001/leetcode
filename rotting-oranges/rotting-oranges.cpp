47fcaf9df75ec61dd06351f1494d68fe7f1bcbc4
naman goel <51822476+namangoel001@users.noreply.github.com>
2021-09-17 04:46:11 +0530

    Time: 10 ms (29.82%), Space: 13.2 MB (35.60%) - LeetHub

--- rotting-oranges/rotting-oranges.cpp
+++ rotting-oranges/rotting-oranges.cpp
@@ -1,55 +1,45 @@
 class Solution {
 public:
-     int orangesRotting(vector<vector<int>>& grid) {
-        
-        queue<pair<int,int>>que;
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int,int>> q;
         int min_time=0;
-        
-        int row= grid.size();
-        int col= grid[0].size();
-        
-        for(int i=0; i<row; i++)
+        for(int i=0;i<grid.size();i++)
         {
-            for(int j=0; j<col; j++)
+            for(int j=0;j<grid[0].size();j++)
             {
                 if(grid[i][j]==2)
-                    que.push({i,j});
+                    q.push({i,j});
             }
         }
-        
-        vector<vector<int>>arr={ {1,0}, {-1,0}, {0,1}, {0,-1} };
-        
-        while(!que.empty())
+        vector<vector<int>> arr={{1,0},{-1,0},{0,-1},{0,1}};
+        while(!q.empty())
         {
-            int l=que.size();
+            int len=q.size();
             min_time++;
-            for(int i=1; i<=l; i++)
+            for(int i=0;i<len;i++)
             {
-                auto[o,j]= que.front();
-                que.pop();
-                
-                for(int k=0; k<4; k++)
+                pair<int,int> p=q.front();
+                q.pop();
+                for(int k=0;k<4;k++)
                 {
-                    int x= o+arr[k][0];
-                    int y= j+arr[k][1];
-                    
-                    if(x>=0 && x<row && y>=0 && y<col && grid[x][y]==1)
-                    {   
+                    int x=p.first+arr[k][0];
+                    int y=p.second+arr[k][1];
+                    if(x>=0 && y>=0 && x<grid.size() && y<grid[0].size() && grid[x][y]==1)
+                    {
                         grid[x][y]=2;
-                        que.push({x,y});
+                        q.push({x,y});
                     }
                 }
             }
         }
-        
-        for(int i=0; i<row; i++)
+        for(int i=0;i<grid.size();i++)
         {
-            for(int j=0; j<col; j++)
+            for(int j=0;j<grid[0].size();j++)
             {
                 if(grid[i][j]==1)
                     return -1;
             }
         }
-        return min_time==0?min_time:min_time-1;
+        return min_time==0?min_time :min_time-1;
     }
 };
