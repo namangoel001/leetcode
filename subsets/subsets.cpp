46064a009531835bbcc20ad67f6f00e230276e3e
naman goel <51822476+namangoel001@users.noreply.github.com>
2021-10-05 01:24:58 +0530

    Time: 4 ms (56.59%), Space: 16 MB (12.06%) - LeetHub

---
+++ subsets/subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void make_sub(vector<vector<int>> &ans,vector<int> v,vector<int> &nums,int start)
+    {
+     if(start==nums.size())
+     {
+         ans.push_back(v);
+         return;
+     }
+        make_sub(ans,v,nums,start+1);
+        v.push_back(nums[start]);
+        make_sub(ans,v,nums,start+1);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> v;
+        vector<vector<int>> ans;
+//         sort(nums.begin(),nums.end());
+        make_sub(ans,v,nums,0);
+        return ans;
+    }
+};
