c6815a255b793b79feb3eb95e1aedf822b5c42b1
naman goel <51822476+namangoel001@users.noreply.github.com>
2021-10-10 15:26:30 +0530

    Time: 44 ms (37.07%), Space: 27.4 MB (5.19%) - LeetHub

--- next-greater-element-ii/next-greater-element-ii.cpp
+++ next-greater-element-ii/next-greater-element-ii.cpp
@@ -7,25 +7,25 @@ public:
          vec.push_back(nums[i]);
         stack<int> s;
         vector<int> res;
-        for(int i=vec.size()-1;i>=0;i--)
-        {
-            if(s.size()==0)
-                res.push_back(-1);
-            else if(s.top()>vec[i])
-                res.push_back(s.top());
-            else if(s.top()<=vec[i])
-            {
-                while(s.size()>0 && s.top()<=vec[i])
-                    s.pop();
-                if(s.size()==0)
-                    res.push_back(-1);
-                else
-                    res.push_back(s.top());
-            }
-            s.push(vec[i]);
-        }
-        reverse(res.begin(),res.end());
-        res.erase(res.begin()+nums.size(),res.end());
+     for(int i=vec.size()-1;i>=0;i--)
+     {
+         if(s.size()==0)
+             res.push_back(-1);
+         else if(s.top()>vec[i])
+             res.push_back(s.top());
+         else if(s.top()<=vec[i])
+         {
+             while(s.size()>0 && s.top()<=vec[i])
+                 s.pop();
+             if(s.size()==0)
+                 res.push_back(-1);
+             else
+                 res.push_back(s.top());
+         }
+         s.push(vec[i]);
+     }
+     reverse(res.begin(),res.end());
+     res.erase(res.begin()+nums.size(),res.end());
         return res;
     }
 };
